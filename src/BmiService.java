78ca9924a7b5d111cb3c07f4f746df17bdb50632
Ekaterina Romanova <Dekaterina83@mail.ru>
2024-10-31 19:45:16 +0300

    BmiService changet

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,9 +1,8 @@
 public class BmiService {
 
-    public double calculate(double weight, double height) {
+    public int calculate(int weight, double height) {
         double index = weight / (height * height);
-        int result = (int) index;
-        return result;
+        return (int) index;
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double weight = 65.5; // вес в килограммах
+        int weight = 65; // вес в килограммах
         double height = 1.7; // рост в метрах
-        double index = service.calculate(weight, height );
+        int index = service.calculate(weight, height );
         System.out.println(index);
     }
 }
